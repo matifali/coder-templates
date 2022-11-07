5ca8959e14716ae8c234968fa57d6b3160c9df87
Muhammad Atif Ali <matifali@live.com>
2022-11-08 00:37:50 +0300

    Updated terraform providers, 4 options 4 deeplearning images

--- deeplearning/main.tf
+++ deeplearning/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "0.5.3"
+      version = "0.6.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.22.0"
+      version = "2.23.0"
     }
   }
 }
@@ -25,13 +25,6 @@ variable "OS" {
   What operating system is your Coder host on?
   EOF
   sensitive = true
-}
-
-locals {
-  jupyter-type-arg = "${var.jupyter == "notebook" ? "Notebook" : "Server"}"
-  tensorflow-version = "${var.tensorflow_version == "latest" ? "" : "${var.tensorflow_version}"}"
-  docker-file-name = "${var.conda_selection == "Yes" ? "Dockerfile.conda" : "Dockerfile"}"
-  jupyter-path = "${var.conda_selection == "Yes" ? "/home/${data.coder_workspace.me.owner}/.conda/envs/DL/bin/" : "/home/${data.coder_workspace.me.owner}/.local/bin/"}"
 }
 
 variable "python_version" {
@@ -60,16 +53,18 @@ variable "tensorflow_version" {
 }
 }
 
-variable "conda_selection" {
-  description = "Do you need conda environment? (everything will work without it too)"
-  default     = "No"
+variable "environmnet_type" {
+  description = "Which environment type do you want to create?"
+  default     = "Full"
   validation {
     condition = contains([
-      "No",
-      "Yes"
-    ], var.conda_selection)
+      "Full",
+      "Full with conda",
+      "PyTorch",
+      "Tensorflow"
+    ], var.environmnet_type)
     error_message = "Not supported!"   
-}
+  }
 }
 
 variable "jupyter" {
@@ -82,6 +77,15 @@ variable "jupyter" {
     ], var.jupyter)
     error_message = "Invalid Jupyter!"   
 }
+}
+
+variable "cpu" {
+  description = "How many CPU cores for this workspace?"
+  default     = "08"
+  validation {
+    condition     = contains(["08", "16", "32"], var.cpu) # this will show a picker
+    error_message = "Invalid CPU count!"
+  }
 }
 
 variable "ram" {
@@ -103,12 +107,28 @@ provider "coder" {
 data "coder_workspace" "me" {
 }
 
+locals {
+  jupyter-type-arg = "${var.jupyter == "notebook" ? "Notebook" : "Server"}"
+  tensorflow-version = "${var.tensorflow_version == "latest" ? "" : "${var.tensorflow_version}"}"
+  jupyter-path = "${var.environmnet_type == "Full with conda" ? "/home/${data.coder_workspace.me.owner}/.conda/envs/DL/bin/" : "/home/${data.coder_workspace.me.owner}/.local/bin/"}"
+  docker-image-file = "${var.environmnet_type == "Full" ? "no-conda.Dockerfile" : var.environmnet_type == "Full with conda" ? "conda.Dockerfile" : var.environmnet_type == "PyTorch" ? "pytorch.Dockerfile" : "tensorflow.Dockerfile"}"
+}
+
 # jupyter
 resource "coder_app" "jupyter" {
   agent_id      = coder_agent.dev.id
   name          = "jupyter-${var.jupyter}"
+  slug          = "jupyter-${var.jupyter}"
   icon          = "https://cdn.icon-icons.com/icons2/2667/PNG/512/jupyter_app_icon_161280.png"
   url           = "http://localhost:8888/@${data.coder_workspace.me.owner}/${lower(data.coder_workspace.me.name)}/apps/jupyter-${var.jupyter}/"
+  subdomain     = false
+  share         = owner
+
+  healthcheck {
+    url = "http://localhost:8888/@${data.coder_workspace.me.owner}/${lower(data.coder_workspace.me.name)}/apps/jupyter-${var.jupyter}/healthz"
+    interval = 5
+    threshold = 10
+  }
 }
 
 resource "coder_agent" "dev" {
@@ -127,14 +147,14 @@ EOT
 }
 
 resource "docker_volume" "home_volume" {
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-home"
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-root"
 }
 
 resource "docker_image" "deeplearning" {
-  name = "${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
+  name = "coder-dockerdl-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
   build {
     path       = "./images/"
-    dockerfile = "${local.docker-file-name}"
+    dockerfile = "${local.docker-image-file}"
     tag        = ["matifali/deeplearning:latest"]
     build_arg = {
       USERNAME = "${data.coder_workspace.me.owner}"
@@ -143,14 +163,15 @@ resource "docker_image" "deeplearning" {
     }
   }
   # Keep alive for other workspaces to use upon deletion
-  keep_locally = false
+  keep_locally = true
 }
 
 resource "docker_container" "workspace" {
   count = data.coder_workspace.me.start_count
   image = docker_image.deeplearning.image_id
+  cpu_shares = var.cpu
   memory = "${var.ram*1024}"
-  gpus = "all"
+  runtime = "nvidia"
   # Uses lower() to avoid Docker restriction on container names.
   name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
   # Hostname makes the shell more user friendly: coder@my-workspace:~$
@@ -172,7 +193,7 @@ resource "docker_container" "workspace" {
   }
   # users home directory
   volumes {
-    container_path = "/home/${data.coder_workspace.me.owner}"
+  	container_path = "/home/${data.coder_workspace.me.owner}"
     volume_name    = docker_volume.home_volume.name
     read_only      = false
   }
@@ -180,6 +201,6 @@ resource "docker_container" "workspace" {
   volumes {
     container_path = "/home/${data.coder_workspace.me.owner}/share"
     host_path      = "/data/share/"
-    read_only      = true
+    read_only      = false
   }
 }
--- matlab/main.tf
+++ matlab/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.22.0"
+      version = "2.23.0"
     }
   }
 }
@@ -26,6 +26,14 @@ variable "OS" {
   sensitive = true
 }
 
+variable "cpu" {
+  description = "How many CPU cores for this workspace?"
+  default     = "10"
+  validation {
+    condition = contains(["05", "10", "20", "30", "40"], var.cpu)
+    error_message = "value must be one of the options"
+  }
+}
 variable "ram" {
   description = "How much RAM for your workspace? (min: 32 GB, max: 128 GB)"
   default     = "32"
@@ -48,9 +56,12 @@ data "coder_workspace" "me" {
 # Matlab
 resource "coder_app" "matlab" {
   agent_id = coder_agent.dev.id
-  name     = "Matlab"
+  name     = "matlab"
+  slug     = "matlab"
   icon     = "https://img.icons8.com/nolan/344/matlab.png"
-  url      = "http://localhost:8888/@${data.coder_workspace.me.owner}/${data.coder_workspace.me.name}/apps/Matlab"
+  url      = "http://localhost:8888/@${data.coder_workspace.me.owner}/${data.coder_workspace.me.name}/apps/matlab"
+  subdomain = false
+  share     = owner
 }
 
 
@@ -86,15 +97,15 @@ variable "docker_image" {
 }
 
 resource "docker_volume" "home_volume" {
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-home"
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-root"
 }
 
 resource "docker_image" "coder_image" {
-  name = "${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
+  name = "coder-base-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
   build {
     path       = "./images/"
     dockerfile = "${var.docker_image}.Dockerfile"
-    tag        = ["coder-${var.docker_image}:latest"]
+    tag        = ["coder-${var.docker_image}:v0.1"]
   }
 
   # Keep alive for other workspaces to use upon deletion
@@ -104,6 +115,7 @@ resource "docker_image" "coder_image" {
 resource "docker_container" "workspace" {
   count = data.coder_workspace.me.start_count
   image = docker_image.coder_image.image_id
+  cpu_shares = var.cpu
   memory = "${var.ram*1024}"
   # Uses lower() to avoid Docker restriction on container names.
   name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
@@ -117,6 +129,7 @@ resource "docker_container" "workspace" {
   stdin_open = true
   tty = true
   env = ["CODER_AGENT_TOKEN=${coder_agent.dev.token}"]
+
   host {
     host = "host.docker.internal"
     ip   = "host-gateway"
@@ -131,6 +144,6 @@ resource "docker_container" "workspace" {
   volumes {
     container_path = "/home/matlab/share"
     host_path      = "/data/share"
-    read_only      = true
+    read_only      = false
   }
 }
