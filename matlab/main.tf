202c4c895bdd087f109c3f11872cb0ab2ef31060
Muhammad Atif Ali <matifali@live.com>
2023-01-12 10:40:28 +0300

    Mounted more volumes

--- matlab/main.tf
+++ matlab/main.tf
@@ -47,10 +47,10 @@ data "coder_workspace" "me" {
 
 # Matlab
 resource "coder_app" "matlab_browser" {
-  agent_id     = coder_agent.dev.id
+  agent_id     = coder_agent.main.id
   display_name = "Matlab Browser"
   slug         = "matlab"
-  icon         = "https://img.icons8.com/nolan/344/matlab.png"
+  icon         = "/icon/matlab.svg"
   url          = "http://localhost:8888"
   subdomain    = true
   share        = "owner"
@@ -58,56 +58,36 @@ resource "coder_app" "matlab_browser" {
 
 
 resource "coder_app" "matlab_desktop" {
-  agent_id     = coder_agent.dev.id
+  agent_id     = coder_agent.main.id
   display_name = "MATLAB Desktop"
   slug         = "desktop"
-  icon         = "https://img.icons8.com/nolan/344/matlab.png"
+  icon         = "/icon/matlab.svg"
   url          = "http://localhost:6080"
   subdomain    = true
   share        = "owner"
 }
 
-resource "coder_agent" "dev" {
+resource "coder_agent" "main" {
   arch           = var.arch
   os             = "linux"
   startup_script = <<EOT
-#!/bin/bash
-set -euo pipefail
-# make user share directory
-mkdir -p ~/share
-# Add matlab to PATH
-export PATH=/opt/matlab/`ls /opt/matlab | grep R*`/bin:$PATH
-# start Matlab browser
-/bin/run.sh -browser 2>&1 | tee ~/matlab_browser.log &
-# start desktop
-/bin/run.sh -vnc 2>&1 | tee ~/matlab.log &
-  EOT
-}
+    #!/bin/bash
+    set -euo pipefail
+    # make user share directory
+    mkdir -p ~/share
+    # Add matlab to PATH
+    export PATH=/opt/matlab/`ls /opt/matlab | grep R*`/bin:$PATH
+    # start Matlab browser
+    /bin/run.sh -browser 2>&1 | tee ~/matlab_browser.log &
+    # start desktop
+    /bin/run.sh -vnc 2>&1 | tee ~/matlab.log &
+    EOT
 
-resource "docker_volume" "home_volume" {
-  name = "coder-${data.coder_workspace.me.id}-home"
-  # Protect the volume from being deleted due to changes in attributes.
-  lifecycle {
-    ignore_changes = all
-  }
-  # Add labels in Docker to keep track of orphan resources.
-  labels {
-    label = "coder.owner"
-    value = data.coder_workspace.me.owner
-  }
-  labels {
-    label = "coder.owner_id"
-    value = data.coder_workspace.me.owner_id
-  }
-  labels {
-    label = "coder.workspace_id"
-    value = data.coder_workspace.me.id
-  }
-  # This field becomes outdated if the workspace is renamed but can
-  # be useful for debugging or cleaning out dangling volumes.
-  labels {
-    label = "coder.workspace_name_at_creation"
-    value = data.coder_workspace.me.name
+  env = {
+    GIT_AUTHOR_NAME     = "${data.coder_workspace.me.owner}"
+    GIT_COMMITTER_NAME  = "${data.coder_workspace.me.owner}"
+    GIT_AUTHOR_EMAIL    = "${data.coder_workspace.me.owner_email}"
+    GIT_COMMITTER_EMAIL = "${data.coder_workspace.me.owner_email}"
   }
 }
 
@@ -119,6 +99,87 @@ resource "docker_image" "matlab" {
   name          = data.docker_registry_image.matlab.name
   pull_triggers = [data.docker_registry_image.matlab.sha256_digest]
   keep_locally  = true
+}
+
+#Volumes Resources
+
+#home_volume
+resource "docker_volume" "home_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-home"
+}
+resource "coder_metadata" "home_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.home_volume.id
+  hide        = true
+  item {
+    key   = "home_volume"
+    value = docker_volume.home_volume.mountpoint
+  }
+}
+#usr_volume
+resource "docker_volume" "usr_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-usr"
+}
+resource "coder_metadata" "usr_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.usr_volume.id
+  hide        = true
+  item {
+    key   = "usr_volume"
+    value = docker_volume.usr_volume.mountpoint
+  }
+}
+#var_volume
+resource "docker_volume" "var_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-var"
+}
+resource "coder_metadata" "var_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.var_volume.id
+  hide        = true
+  item {
+    key   = "var_volume"
+    value = docker_volume.var_volume.mountpoint
+  }
+}
+#etc_volume
+resource "docker_volume" "etc_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-etc"
+}
+resource "coder_metadata" "etc_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.etc_volume.id
+  hide        = true
+  item {
+    key   = "etc_volume"
+    value = docker_volume.etc_volume.mountpoint
+  }
+}
+#opt_volume
+resource "docker_volume" "opt_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-opt"
+}
+resource "coder_metadata" "opt_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.opt_volume.id
+  hide        = true
+  item {
+    key   = "etc_volume"
+    value = docker_volume.opt_volume.mountpoint
+  }
+}
+#root_volume
+resource "docker_volume" "root_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-root"
+}
+resource "coder_metadata" "root_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.root_volume.id
+  hide        = true
+  item {
+    key   = "root_volume"
+    value = docker_volume.root_volume.mountpoint
+  }
 }
 
 resource "docker_container" "workspace" {
@@ -134,8 +195,8 @@ resource "docker_container" "workspace" {
   hostname = lower(data.coder_workspace.me.name)
   dns      = ["1.1.1.1"]
   # Use the docker gateway if the access URL is 127.0.0.1 
-  entrypoint = ["sh", "-c", replace(coder_agent.dev.init_script, "127.0.0.1", "host.docker.internal")]
-  env        = ["CODER_AGENT_TOKEN=${coder_agent.dev.token}"]
+  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "127.0.0.1", "host.docker.internal")]
+  env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
 
   host {
     host = "host.docker.internal"
@@ -143,22 +204,48 @@ resource "docker_container" "workspace" {
   }
 
   shm_size = 512
+
   # users home directory
   volumes {
-    container_path = "/home/matlab"
+    container_path = "/home/coder"
     volume_name    = docker_volume.home_volume.name
     read_only      = false
   }
-  # personal data directory
   volumes {
-    container_path = "/home/matlab/data"
-    host_path      = "/data/${data.coder_workspace.me.owner}"
+    container_path = "/usr/"
+    volume_name    = docker_volume.usr_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/var/"
+    volume_name    = docker_volume.var_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/etc/"
+    volume_name    = docker_volume.etc_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/opt/"
+    volume_name    = docker_volume.opt_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/root/"
+    volume_name    = docker_volume.root_volume.name
+    read_only      = false
+  }
+  # users data directory
+  volumes {
+    container_path = "/home/coder/data/"
+    host_path      = "/data/${data.coder_workspace.me.owner}/"
     read_only      = false
   }
   # shared data directory
   volumes {
-    container_path = "/home/matlab/share"
-    host_path      = "/data/share"
+    container_path = "/home/coder/share"
+    host_path      = "/data/share/"
     read_only      = false
   }
 
