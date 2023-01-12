8e00bcfd5a34a06cff444fd892427a845ebb8e89
Muhammad Atif Ali <matifali@live.com>
2023-01-12 10:25:33 +0300

    Mounted more volumes

--- deeplearning/main.tf
+++ deeplearning/main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "0.6.6"
     }
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "2.25.0"
     }
   }
@@ -90,17 +90,17 @@ locals {
 
 # jupyter
 resource "coder_app" "jupyter" {
-  agent_id     = coder_agent.dev.id
+  agent_id     = coder_agent.main.id
   display_name = "Jupyter"
   slug         = "jupyter-${var.jupyter}"
-  icon         = "https://cdn.icon-icons.com/icons2/2667/PNG/512/jupyter_app_icon_161280.png"
+  icon         = "/icon/jupyter.svg"
   url          = "http://localhost:8888/"
   subdomain    = true
   share        = "owner"
 }
 
 resource "coder_app" "code-server" {
-  agent_id = coder_agent.dev.id
+  agent_id = coder_agent.main.id
 
   display_name = "VSCode"
   slug         = "code-server"
@@ -110,47 +110,27 @@ resource "coder_app" "code-server" {
   share        = "owner"
 }
 
-resource "coder_agent" "dev" {
+resource "coder_agent" "main" {
   arch           = var.arch
   os             = "linux"
   startup_script = <<EOT
-#!/bin/bash
-set -euo pipefail
-# Create user data directory
-mkdir -p ~/data
-# make user share directory
-mkdir -p ~/share
-# start jupyter
-${local.jupyter-path}/jupyter ${var.jupyter} --no-browser --${local.jupyter-type-arg}App.token='' --ip='*' 2>&1 | tee -a ~/build.log &
-# start code-server
-code-server --accept-server-license-terms serve-local --without-connection-token --quality stable --telemetry-level off 2>&1 | tee -a ~/code-server.log &
-EOT
-}
+    #!/bin/bash
+    set -euo pipefail
+    # Create user data directory
+    mkdir -p ~/data
+    # make user share directory
+    mkdir -p ~/share
+    # start jupyter
+    ${local.jupyter-path}/jupyter ${var.jupyter} --no-browser --${local.jupyter-type-arg}App.token='' --ip='*' 2>&1 | tee -a ~/build.log &
+    # start code-server
+    code-server --accept-server-license-terms serve-local --without-connection-token --quality stable --telemetry-level off 2>&1 | tee -a ~/code-server.log &
+    EOT
 
-resource "docker_volume" "home_volume" {
-  name = "coder-${data.coder_workspace.me.id}-home"
-  # Protect the volume from being deleted due to changes in attributes.
-  lifecycle {
-    ignore_changes = all
-  }
-  # Add labels in Docker to keep track of orphan resources.
-  labels {
-    label = "coder.owner"
-    value = data.coder_workspace.me.owner
-  }
-  labels {
-    label = "coder.owner_id"
-    value = data.coder_workspace.me.owner_id
-  }
-  labels {
-    label = "coder.workspace_id"
-    value = data.coder_workspace.me.id
-  }
-  # This field becomes outdated if the workspace is renamed but can
-  # be useful for debugging or cleaning out dangling volumes.
-  labels {
-    label = "coder.workspace_name_at_creation"
-    value = data.coder_workspace.me.name
+  env = {
+    GIT_AUTHOR_NAME     = "${data.coder_workspace.me.owner}"
+    GIT_COMMITTER_NAME  = "${data.coder_workspace.me.owner}"
+    GIT_AUTHOR_EMAIL    = "${data.coder_workspace.me.owner_email}"
+    GIT_COMMITTER_EMAIL = "${data.coder_workspace.me.owner_email}"
   }
 }
 
@@ -166,6 +146,87 @@ resource "docker_image" "dockerdl" {
   keep_locally = true
 }
 
+#Volumes Resources
+
+#home_volume
+resource "docker_volume" "home_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-home"
+}
+resource "coder_metadata" "home_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.home_volume.id
+  hide        = true
+  item {
+    key   = "home_volume"
+    value = docker_volume.home_volume.mountpoint
+  }
+}
+#usr_volume
+resource "docker_volume" "usr_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-usr"
+}
+resource "coder_metadata" "usr_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.usr_volume.id
+  hide        = true
+  item {
+    key   = "usr_volume"
+    value = docker_volume.usr_volume.mountpoint
+  }
+}
+#var_volume
+resource "docker_volume" "var_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-var"
+}
+resource "coder_metadata" "var_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.var_volume.id
+  hide        = true
+  item {
+    key   = "var_volume"
+    value = docker_volume.var_volume.mountpoint
+  }
+}
+#etc_volume
+resource "docker_volume" "etc_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-etc"
+}
+resource "coder_metadata" "etc_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.etc_volume.id
+  hide        = true
+  item {
+    key   = "etc_volume"
+    value = docker_volume.etc_volume.mountpoint
+  }
+}
+#opt_volume
+resource "docker_volume" "opt_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-opt"
+}
+resource "coder_metadata" "opt_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.opt_volume.id
+  hide        = true
+  item {
+    key   = "etc_volume"
+    value = docker_volume.opt_volume.mountpoint
+  }
+}
+#root_volume
+resource "docker_volume" "root_volume" {
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}-root"
+}
+resource "coder_metadata" "root_volume" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_volume.root_volume.id
+  hide        = true
+  item {
+    key   = "root_volume"
+    value = docker_volume.root_volume.mountpoint
+  }
+}
+
 resource "docker_container" "workspace" {
   count      = data.coder_workspace.me.start_count
   image      = docker_image.dockerdl.image_id
@@ -178,8 +239,8 @@ resource "docker_container" "workspace" {
   hostname = lower(data.coder_workspace.me.name)
   dns      = ["1.1.1.1"]
   # Use the docker gateway if the access URL is 127.0.0.1
-  command = ["sh", "-c", replace(coder_agent.dev.init_script, "127.0.0.1", "host.docker.internal")]
-  env     = ["CODER_AGENT_TOKEN=${coder_agent.dev.token}"]
+  command = ["sh", "-c", replace(coder_agent.main.init_script, "127.0.0.1", "host.docker.internal")]
+  env     = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
 
   host {
     host = "host.docker.internal"
@@ -187,16 +248,42 @@ resource "docker_container" "workspace" {
   }
 
   ipc_mode = "host" # required for PyTorch with multiple workers
-  # users data directory
-  volumes {
-    container_path = "/home/coder/data/"
-    host_path      = "/data/${data.coder_workspace.me.owner}/"
-    read_only      = false
-  }
+
   # users home directory
   volumes {
     container_path = "/home/coder"
     volume_name    = docker_volume.home_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/usr/"
+    volume_name    = docker_volume.usr_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/var/"
+    volume_name    = docker_volume.var_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/etc/"
+    volume_name    = docker_volume.etc_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/opt/"
+    volume_name    = docker_volume.opt_volume.name
+    read_only      = false
+  }
+  volumes {
+    container_path = "/root/"
+    volume_name    = docker_volume.root_volume.name
+    read_only      = false
+  }
+  # users data directory
+  volumes {
+    container_path = "/home/coder/data/"
+    host_path      = "/data/${data.coder_workspace.me.owner}/"
     read_only      = false
   }
   # shared data directory
@@ -205,6 +292,8 @@ resource "docker_container" "workspace" {
     host_path      = "/data/share/"
     read_only      = false
   }
+
+
 
   # Add labels in Docker to keep track of orphan resources.
   labels {
