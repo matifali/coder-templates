a9f9e87cd7f3b6f61fb5b8984e2905cb6b7c693b
Muhammad Atif Ali <matifali@live.com>
2023-08-06 16:05:30 +0300

    update

--- k8s-namespace/main.tf
+++ k8s-namespace/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "~> 0.11.0"
+      version = "~> 0.11"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -11,151 +11,79 @@ terraform {
   }
 }
 
-provider "coder" {
-}
-
-variable "use_kubeconfig" {
-  type        = bool
-  description = <<-EOF
-  Use host kubeconfig? (true/false)
-  EOF
-  default     = false
-}
-
-variable "namespace" {
-  type        = string
-  description = "The Kubernetes namespace to create workspaces in (must exist prior to creating workspaces)"
-}
-
-data "coder_parameter" "cpu" {
-  name         = "cpu"
-  display_name = "CPU"
-  description  = "The number of CPU cores"
-  default      = "2"
-  icon         = "/icon/memory.svg"
-  mutable      = true
-  option {
-    name  = "2 Cores"
-    value = "2"
-  }
-  option {
-    name  = "4 Cores"
-    value = "4"
-  }
-  option {
-    name  = "6 Cores"
-    value = "6"
-  }
-  option {
-    name  = "8 Cores"
-    value = "8"
-  }
-}
-
-data "coder_parameter" "memory" {
-  name         = "memory"
-  display_name = "Memory"
-  description  = "The amount of memory in GB"
-  default      = "2"
-  icon         = "/icon/memory.svg"
-  mutable      = true
-  option {
-    name  = "2 GB"
-    value = "2"
-  }
-  option {
-    name  = "4 GB"
-    value = "4"
-  }
-  option {
-    name  = "6 GB"
-    value = "6"
-  }
-  option {
-    name  = "8 GB"
-    value = "8"
-  }
-}
-
-data "coder_parameter" "home_disk_size" {
-  name         = "home_disk_size"
-  display_name = "Home disk size"
-  description  = "The size of the home disk in GB"
-  default      = "10"
-  type         = "number"
-  icon         = "/emojis/1f4be.png"
-  mutable      = false
-  validation {
-    min = 1
-    max = 100
-  }
-}
-
 provider "kubernetes" {
-  config_path = var.use_kubeconfig == true ? "~/.kube/config" : null
+  config_path = "~/.kube/config"
 }
 
 data "coder_workspace" "me" {}
 
-resource "coder_agent" "main" {
-  os                     = "linux"
-  arch                   = "amd64"
-  startup_script_timeout = 180
-  startup_script         = <<-EOT
-    set -e
-    # code-server
-    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
-    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
-    # vscode-server
-    mkdir -p /tmp/vscode-server
-    HASH=$(curl https://update.code.visualstudio.com/api/commits/stable/server-linux-x64-web | cut -d '"' -f 2)
-    wget -O- https://az764295.vo.msecnd.net/stable/$HASH/vscode-server-linux-x64-web.tar.gz | tar -xz -C /tmp/vscode-server --strip-components=1 >/dev/null 2>&1
-    /tmp/vscode-server/bin/code-server --accept-server-license-terms serve-local --without-connection-token --port 13338 --telemetry-level off >/dev/null 2>&1 &
-  EOT
-  metadata {
-    display_name = "CPU Usage"
-    key          = "0_cpu_usage"
-    script       = "coder stat cpu"
-    interval     = 10
-    timeout      = 1
-  }
-  metadata {
-    display_name = "RAM Usage"
-    key          = "1_ram_usage"
-    script       = "coder stat mem"
-    interval     = 10
-    timeout      = 1
-  }
-  metadata {
-    display_name = "Home Disk"
-    key          = "3_home_disk"
-    script       = "coder stat disk --path $HOME"
-    interval     = 60
-    timeout      = 1
-  }
-  metadata {
-    display_name = "CPU Usage (Host)"
-    key          = "4_cpu_usage_host"
-    script       = "coder stat cpu --host"
-    interval     = 10
-    timeout      = 1
-  }
-  metadata {
-    display_name = "Memory Usage (Host)"
-    key          = "5_mem_usage_host"
-    script       = "coder stat mem --host"
-    interval     = 10
-    timeout      = 1
+# Used for all resources created by this template
+locals {
+  name = "coder-ws-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}"
+  labels = {
+    "app.kubernetes.io/managed-by" = "coder"
   }
 }
 
-# code-server
+resource "kubernetes_namespace" "workspace" {
+  metadata {
+    name   = local.name
+    labels = local.labels
+  }
+}
+
+# ServiceAccount for the workspace
+resource "kubernetes_service_account" "workspace_service_account" {
+  metadata {
+    name      = local.name
+    namespace = kubernetes_namespace.workspace.metadata[0].name
+    labels    = local.labels
+  }
+}
+
+# Gives the ServiceAccount admin access to the
+# namespace created for this workspace
+resource "kubernetes_role_binding" "set_workspace_permissions" {
+  metadata {
+    name      = local.name
+    namespace = kubernetes_namespace.workspace.metadata[0].name
+    labels    = local.labels
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.workspace_service_account.metadata[0].name
+    namespace = kubernetes_namespace.workspace.metadata[0].name
+  }
+}
+
+# The Coder agent allows the workspace owner
+# to connect to the pod from a web or local IDE
+resource "coder_agent" "primary" {
+  os   = "linux"
+  arch = "amd64"
+  startup_script_timeout = 180
+  startup_script         = <<-EOT
+    set -e
+
+    # install and start code-server
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.8.3
+    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+  EOT
+}
+
+# Adds the "VS Code Web" icon to the dashboard
+# and proxies code-server running on the workspace
 resource "coder_app" "code-server" {
-  agent_id     = coder_agent.main.id
+  agent_id     = coder_agent.primary.id
+  display_name = "VS Code Web"
   slug         = "code-server"
-  display_name = "code-server"
+  url          = "http://localhost:13337/"
   icon         = "/icon/code.svg"
-  url          = "http://localhost:13337?folder=/home/coder"
   subdomain    = false
   share        = "owner"
 
@@ -166,88 +94,72 @@ resource "coder_app" "code-server" {
   }
 }
 
-# vscode-server
-resource "coder_app" "vscode-server" {
-  agent_id     = coder_agent.main.id
-  display_name = "VS Code Web"
-  slug         = "vscode-server"
-  url          = "http://localhost:13338?folder=/home/coder"
-  icon         = "https://raw.githubusercontent.com/matifali/logos/main/code.svg"
-  subdomain    = true
-  share        = "owner"
-}
+# Creates a pod on the workspace namepace, allowing
+# the developer to connect.
+resource "kubernetes_pod" "primary" {
 
-resource "kubernetes_persistent_volume_claim" "home" {
+  # Pod is ephemeral. Re-created when a workspace starts/stops.
+  count = data.coder_workspace.me.start_count
+
   metadata {
-    name      = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}-home"
-    namespace = var.namespace
+    name      = "primary"
+    namespace = kubernetes_namespace.workspace.metadata[0].name
+    labels    = local.labels
   }
   spec {
-    access_modes = ["ReadWriteOnce"]
-    resources {
-      requests = {
-        storage = "${data.coder_parameter.home_disk_size.value}Gi"
+    service_account_name = kubernetes_service_account.workspace_service_account.metadata[0].name
+    security_context {
+      run_as_user = "1000"
+      fs_group    = "1000"
+    }
+    container {
+
+      # Basic image with helm, kubectl, etc
+      # extend to add your own tools!
+      image = "bencdr/devops-tools"
+
+      image_pull_policy = "Always"
+      name              = "dev"
+
+      # Starts the Coder agent
+      command = ["sh", "-c", coder_agent.primary.init_script]
+      env {
+        name  = "CODER_AGENT_TOKEN"
+        value = coder_agent.primary.token
+      }
+
+      # Mounts /home/coder. Developers should keep
+      # their files here!
+      volume_mount {
+        mount_path = "/home/coder"
+        name       = "home"
+        read_only  = false
+      }
+    }
+    volume {
+      name = "home"
+      persistent_volume_claim {
+        claim_name = kubernetes_persistent_volume_claim.home.metadata.0.name
+        read_only  = false
       }
     }
   }
 }
 
-resource "kubernetes_service_account" "namespace_service_account" {
+# Creates a persistent volume for developers
+# to store their repos/files
+resource "kubernetes_persistent_volume_claim" "home" {
   metadata {
-    name      = "namespace-service-account"
-    namespace = var.namespace
+    name      = "primary-disk"
+    namespace = kubernetes_namespace.workspace.metadata[0].name
+    labels    = local.labels
   }
-
-  automount_service_account_token = true
-}
-
-resource "kubernetes_role" "namespace_full_access" {
-  metadata {
-    name      = "namespace-full-access"
-    namespace = var.namespace
-  }
-  rule {
-    api_groups = [""]
-    resources  = ["*"]
-    verbs      = ["*"]
-  }
-}
-
-resource "kubernetes_role_binding" "namespace_role_binding" {
-  metadata {
-    name      = "namespace-role-binding"
-    namespace = var.namespace
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "Role"
-    name      = kubernetes_role.namespace_full_access.metadata[0].name
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.namespace_service_account.metadata[0].name
-    namespace = var.namespace
-  }
-}
-
-resource "kubernetes_deployment" "main" {
-  metadata {
-    name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
-  }
+  wait_until_bound = false
   spec {
-    replicas = 1
-    template {
-      metadata {
-        labels = {
-          app = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
-        }
-      }
-      spec {
-        container {
-          image = "bencdr/devops-tools:latest"
-          name  = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
-        }
-        service_account_name = kubernetes_service_account.namespace_service_account.metadata[0].name
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "10Gi"
       }
     }
   }
